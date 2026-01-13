1ff5c50f4d072416412c1cb3076460cd009e6597
Mike Vincent <michael.thomas.vincent@gmail.com>
2026-01-13 10:51:08 -0800

    Update default window size for 2:1 card aspect ratio

--- TimerFantasy/TimerFantasyApp.swift
+++ TimerFantasy/TimerFantasyApp.swift
@@ -2,9 +2,9 @@ import SwiftUI
 
 @main
 struct TimerFantasyApp: App {
-    // 2 columns × 1 row with 200pt cards + 8pt spacing
-    private let initialWidth: CGFloat = 2 * 200 + 3 * 8  // 424
-    private let initialHeight: CGFloat = 1 * 200 + 2 * 8 + 28  // 244 (28 for title bar)
+    // 1 card with 2:1 aspect ratio (400x200) + 8pt spacing
+    private let initialWidth: CGFloat = 400 + 2 * 8  // 416
+    private let initialHeight: CGFloat = 200 + 2 * 8 + 28  // 244 (28 for title bar)
 
     var body: some Scene {
         WindowGroup {
